2cfeec42747e749125e33b58cc7a6e6351c05efe
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-11-01 13:46:07 +0330

    feature(auth): set default auth class to simple jwt

--- src/config/settings.py
+++ src/config/settings.py
@@ -137,6 +137,9 @@ AUTH_USER_MODEL = 'core.CoreUser'
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 # SIMPLE JWT
