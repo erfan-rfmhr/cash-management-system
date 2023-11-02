e568a9fde76fe3d80e50f63e7c82ebfe659714c5
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-11-02 17:30:07 +0330

    feat(transaction model): create transaction model

--- src/transactions/models.py
+++ src/transactions/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class TransactionModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
