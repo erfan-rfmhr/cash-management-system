afef2750402d0d63487c07ceedd21347ced868eb
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-11-02 17:55:32 +0330

    feat(transactions app): create transaction viewset

--- src/transactions/views.py
+++ src/transactions/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import TransactionModel
+from .serializers import TransactionSerializer
+
+
+class TransactionViewSet(ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return TransactionModel.objects.filter(user=user)
