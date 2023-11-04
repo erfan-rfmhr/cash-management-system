33efab93113378abcc7819aebc04601025880385
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2023-11-04 13:05:52 +0330

    fix(bad view): fix bad view set for daily report

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -7,6 +7,6 @@ app_name = 'reports'
 urlpatterns = [
     re_path(r'^(?P<year>[0-9]{4})/$', views.AnnualReportView.as_view(), name='annual-report'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.MonthlyReportView.as_view(), name='monthly-report'),
-    re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.MonthlyReportView.as_view(),
+    re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.DailyReportView.as_view(),
             name='daily-report'),
 ]
